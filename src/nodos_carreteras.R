bd3269a01f9d80187d8bffac39c89b76c9537fd5
David Martínez <jdavid.macer@gmail.com>
2021-04-05 20:12:20 -0500

    Agrega script para importar y visualizar los nodos en la carretera

---
+++ src/nodos_carreteras.R
@@ -0,0 +1,25 @@
+# Cargamos las librerías
+library(sf)
+
+# Importo los datos
+tabla_datos_shape <- st_read("av_fuente_integrada.shp")
+
+# Vemos los metadatos del shapefile utilizando las funciones st_geometry_type(), 
+# st_crs() and st_bbox().
+
+# Primero veremos el tipo de geometría
+st_geometry_type(tabla_datos)
+
+# Ahora veremos el sistema de coordenadas de referencia (CRS)
+st_crs(tabla_datos)
+
+# Ahora veremos la extensión de nuestros datos
+st_bbox(tabla_datos)
+
+# Graficar un shapefile
+library(ggplot2)
+
+ggplot() + 
+  geom_sf(data = tabla_datos, size = 2, color = "red") + 
+  ggtitle("Nodos en la carretera") + 
+  coord_sf()
